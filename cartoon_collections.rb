6dc7efd88dc51e387af7f9d750c1052682d87085
Lynden Lim <lyndenlim15@gmail.com>
2022-02-22 15:11:57 -0500

    commit tree 156d6dda7df478e4d9086a4e543a8ac184d985c5

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,31 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarves)
+  puts dwarves.map.with_index { |dwarf, index| "#{index + 1}. #{dwarf}" }
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(elements)
+  elements.map { |element| "#{element.capitalize}!" }
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(calls)
+  calls_array = calls.map { |call|
+    if call.length > 4
+      true
+    else
+      false
+    end
+  }
+  if calls_array.include?(true) 
+    return true
+  else 
+    return false
+  end
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(food_array)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  food_array.find { |food|
+    cheese_types.include?(food)
+  }
 end
+
+
